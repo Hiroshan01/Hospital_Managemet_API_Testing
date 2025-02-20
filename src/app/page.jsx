ca32cd628864a74e4afe77e02b624790bfa42e4c
Hiroshan Madusanka <pmhiroshan@gmail.com>
2025-02-20 10:31:54 +0530

    p tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,6 +10,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-6">Welcome to the App</h1>
       <div className="space-y-4">
        <h1>Home</h1>
+       <p>Testing deply</p>
        <li><Link href="/signIn">Sign In</Link> </li>
        <li><Link href="/signUp">Sign Up</Link></li>
       </div>
